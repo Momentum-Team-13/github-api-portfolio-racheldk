38fe8bfc26baae857ca61e0c7848b13dffde6c46
Rachel Kelly <racheldk126@gmail.com>
2022-06-02 13:45:38 -0700

    image on page

--- main.js
+++ main.js
@@ -12,16 +12,79 @@ fetch(gitHubUrl, {
         return response.json()
     })
     .then(function(data){
-        console.log(data.name)
-        buildProfile(data.name)
+        console.log(data)
+        buildProfile(data)
     })
 
-   function buildProfile(profileData) {
-       let nameElement = document.createElement('h1')
-       nameElement.classList.add('profileCard')
-       nameElement.innerText = `${profileData}` 
-       console.log(`function ${nameElement.innerText}`)
-       page.appendChild(nameElement) 
-   }
+    function buildProfile(profileData) {
+        let nameElement = document.createElement('h1')
+        nameElement.classList.add('profileCard')
+        nameElement.innerText = `${profileData.name}` 
+        console.log(`function ${nameElement.innerText}`)
+        page.appendChild(nameElement) 
 
-   
+        let avatarElement = document.createElement('img')
+            avatarElement.src = profileData.avatar_url
+            avatarElement.alt = "the user's avatar"
+            console.log('loadAvatar')
+            page.appendChild(avatarElement)
+    }   
+
+
+//add Location 
+fetch(gitHubUrl, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+})
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data){
+        addLocation(data.location)
+    })
+
+function addLocation(location) {
+    let locationElement = document.createElement('div')
+    locationElement.innerText = `Location: ${location}`
+    page.appendChild(locationElement)
+}    
+
+//add GitHub URL
+fetch(gitHubUrl, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+})
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        addGitUrl(data.url)
+    })
+
+    function addGitUrl(gitUrl) {
+        let urlElement = document.createElement('div')
+        urlElement.innerText = `GitHub URL: ${gitUrl}`
+        page.appendChild(urlElement)
+    }
+
+
+//add repos
+const repoUrl = "https://api.github.com/users/racheldk/repos"
+fetch(repoUrl, {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/JSON'},
+})    
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        addRepos(data.html_url)
+    })
+
+    function addRepos(reposName){
+        for (let repo of repos){
+            let repoElement = document.createElement('div')
+            repoElement.innerText = repo
+            page.appendChild(repoElement)
+        }
+    }
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 console.log("connected! woo!")
+let profile = document.querySelector('#profile')
 
 // started in insomnia, so we could see things. but now we're going to get javascript to do the thing insomnia was doing 
 
-let gitHubUrl = "https://api.github.com/users/racheldk"
+let gitHubUrl = "https://api.github.com/users/racheldk/repos"
 //could also put this right into your fetch request, but this makes it more readable 
 
 //fetch comes for free with js
@@ -18,15 +19,38 @@ fetch(gitHubUrl, {
     })
     .then(function (data) { 
     //data refers to what the above promise returned (response.json()). it's a multi-step process: you get the response, turn it into json, then you can work with it
-    console.log("Response from GitHub API: ", data)
     //data.name would give you  just the value of the name key from the API
     buildProfile(data)
     })
     // this is pretty boiler-plate. 
 
-    function buildProfile(profileData){
-        console.log(profileData)
-        //create elements and add them to the page
-        //profileData is the data from the promise
-        //making this separate function (that you then call in the fetch) will be a little easier to read and keep track of today
+    // function buildProfile(profileData){
+    //     console.log(profileData)
+    //     //create elements and add them to the page
+    //     //profileData is the data from the promise
+    //     //making this separate function (that you then call in the fetch) will be a little easier to read and keep track of today
+    // }
+
+
+    //afternoon class MAPS
+    function buildProfile(profileData) {
+        names = profileData.map(function (repo) {
+            return repo.name 
+        })
+        console.log(names)
+        elements = names.map(function (name){
+            return buildRepoElement(name)
+        })
+        console.log("elements", elements)
+        for (let element of elements) {
+            profile.appendChild(element)
+        }
+    }
+
+
+    function buildRepoElement(name){
+        let el = document.createElement('p')
+        el.innerText=name 
+        return el
+        //returns a new element for a repo, like a customer
     }
