dcb0b4218dc786f1cd6983b1c535f88474a87fe0
Rachel Kelly <racheldk126@gmail.com>
2022-06-03 09:46:16 -0700

    re-organizing existing code

--- main.js
+++ main.js
@@ -1,90 +1,73 @@
-console.log("connected!")
-const page = document.querySelector('#profile')
+console.log("connected!");
+const page = document.querySelector("#profile");
 
-let gitHubUrl = "https://api.github.com/users/racheldk"
-
+let gitHubUrl = "https://api.github.com/users/racheldk";
 
 fetch(gitHubUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/JSON' },
+  method: "GET",
+  headers: { "Content-Type": "application/JSON" },
 })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        buildProfile(data)
-    })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    buildProfile(data);
+  });
 
-    function buildProfile(profileData) {
-        let nameElement = document.createElement('h1')
-        nameElement.classList.add('profileCard')
-        nameElement.innerText = `${profileData.name}` 
-        console.log(`function ${nameElement.innerText}`)
-        page.appendChild(nameElement) 
+function buildProfile(profileData) {
+  let nameElement = document.createElement("h1");
+  nameElement.classList.add("profileCard");
+  nameElement.innerText = `${profileData.name}`;
+  console.log(`function ${nameElement.innerText}`);
+  page.appendChild(nameElement);
 
-        let avatarElement = document.createElement('img')
-            avatarElement.src = profileData.avatar_url
-            avatarElement.alt = "the user's avatar"
-            console.log('loadAvatar')
-            page.appendChild(avatarElement)
-    }   
+  let avatarElement = document.createElement("img");
+  avatarElement.src = profileData.avatar_url;
+  avatarElement.alt = "the user's avatar";
+  console.log("loadAvatar");
+  page.appendChild(avatarElement);
 
+  let locationElement = document.createElement("div");
+  locationElement.innerText = `Location: ${data.location}`;
+  page.appendChild(locationElement);
 
-//add Location 
-fetch(gitHubUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-})
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data){
-        addLocation(data.location)
-    })
+  let urlElement = document.createElement("div");
+  urlElement.innerText = `GitHub URL: ${data.url}`;
+  page.appendChild(urlElement);
+}
 
-function addLocation(location) {
-    let locationElement = document.createElement('div')
-    locationElement.innerText = `Location: ${location}`
-    page.appendChild(locationElement)
-}    
+// //add GitHub URL
+// fetch(gitHubUrl, {
+//   method: "GET",
+//   headers: { "Content-Type": "application/json" },
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     addGitUrl(data.url);
+//   });
 
-//add GitHub URL
-fetch(gitHubUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-})
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        addGitUrl(data.url)
-    })
-
-    function addGitUrl(gitUrl) {
-        let urlElement = document.createElement('div')
-        urlElement.innerText = `GitHub URL: ${gitUrl}`
-        page.appendChild(urlElement)
-    }
-
+// function addGitUrl(gitUrl) {}
 
 //add repos
-const repoUrl = "https://api.github.com/users/racheldk/repos"
+const repoUrl = "https://api.github.com/users/racheldk/repos";
 fetch(repoUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/JSON'},
-})    
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        addRepos(data.html_url)
-    })
+  method: "GET",
+  headers: { "Content-Type": "application/JSON" },
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    addRepos(data.html_url);
+  });
 
-    function addRepos(reposName){
-        for (let repo of repos){
-            let repoElement = document.createElement('div')
-            repoElement.innerText = repo
-            page.appendChild(repoElement)
-        }
-    }
+function addRepos(reposName) {
+  for (let repo of repos) {
+    let repoElement = document.createElement("div");
+    repoElement.innerText = repo;
+    page.appendChild(repoElement);
+  }
+}
